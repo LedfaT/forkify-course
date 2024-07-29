61bbf4fa37372f6e53555deb37919cc68b0a3a15
David Zaitsev <zaitsevdavid4@gmail.com>
2024-07-29 14:14:00 +0300

    initial

---
+++ src/js/views/searchView.js
@@ -0,0 +1,24 @@
+import View from './view.js';
+
+class SearchView extends View {
+  _parentElement = document.querySelector('.search');
+
+  getQuery() {
+    const query = this._parentElement.querySelector('.search__field').value;
+    this._clearIpunt();
+    return query;
+  }
+
+  _clearIpunt() {
+    this._parentElement.querySelector('.search__field').value = '';
+  }
+
+  addHandlerSearch(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      handler();
+    });
+  }
+}
+
+export default new SearchView();
